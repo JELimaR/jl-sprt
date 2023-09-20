1d5bf5228267858da8e70ee31c4e2dbfe4a7a193
juanlr07 <juanlr07@gmail.com>
2023-09-20 08:24:32 -0400

    eliminar stageCreatorFunc archivo

--- src/Tournament/Tournament.ts
+++ src/Tournament/Tournament.ts
@@ -1,10 +1,9 @@
 import { JDateTime } from "../Calendar/DateTime/JDateTime";
 import JCalendar from "../Calendar/JCalendar";
 import { ITCCConfig, ITCCInfo, TCC } from "../patterns/templateConfigCreator";
-import { IStageConfig } from "./Stage/Stage";
-import stageCreatorFunc from "./Stage/stageCreatorFunc";
-// import StageGroup, { IStageGroupConfig } from "./Stage/StageGroup/StageGroup";
-// import StagePlayoff, { IStagePlayoffConfig } from "./Stage/StagePlayoff/StagePlayoff";
+import { IStageConfig, IStageInfo } from "./Stage/Stage";
+import StageGroup, { IStageGroupConfig } from "./Stage/StageGroup/StageGroup";
+import StagePlayoff, { IStagePlayoffConfig } from "./Stage/StagePlayoff/StagePlayoff";
 import { TGS } from "./types";
 
 export interface ITournamentConfig extends ITCCConfig {
@@ -21,25 +20,23 @@ export default class Tournament extends TCC<ITournamentInfo, ITournamentConfig> 
   constructor(info: ITournamentInfo, config: ITournamentConfig, cal: JCalendar) {
     super(info, config);
     config.stages.forEach((sconfig, i: number) => { 
-      let stage: TGS;
-      stage = stageCreatorFunc({id: `${info.id}_s${i+1}`, season: info.season}, sconfig, cal);
-
+      const stage: TGS = Tournament.create({id: `${info.id}_s${i+1}`, season: info.season}, sconfig, cal);
       this._stages.push(stage);
     })
   }
 
   get stages() { return this._stages }
 
-  // static create(info: IStageInfo, config: IStageConfig, cal: JCalendar): Stage<any, any> {
-  //   if (config.type == 'group') {
-  //     const sconfig = config as IStageGroupConfig;
-  //     return new StageGroup(info, sconfig, cal);
-  //   } else if (config.type == 'playoff') {
-  //     const sconfig = config as IStagePlayoffConfig;
-  //     return new StagePlayoff(info, sconfig, cal);
-  //   } else {
-  //     throw new Error(`not implemented. (en StageConstructor)`)
-  //   }
-  // }
+  static create(info: IStageInfo, config: IStageConfig, cal: JCalendar): TGS {
+    if (config.type == 'group') {
+      const sconfig = config as IStageGroupConfig;
+      return new StageGroup(info, sconfig, cal);
+    } else if (config.type == 'playoff') {
+      const sconfig = config as IStagePlayoffConfig;
+      return new StagePlayoff(info, sconfig, cal);
+    } else {
+      throw new Error(`not implemented. (en StageConstructor)`)
+    }
+  }
 	
 }
