1324c177ae1e617d5ff69a67c17cf37d506dc87f
juanlr07 <juanlr07@gmail.com>
2024-05-14 06:58:51 -0400

    pattern template data and creator

---
+++ src/JSportModule/patterns/templateDataCreator.ts
@@ -0,0 +1,14 @@
+
+
+export interface ITDCData {}
+
+export interface ITDCCreator {}
+
+export abstract class TDC<D extends ITDCData, C extends ITDCCreator> {
+  private _info: C
+  constructor(creator: C) {
+    this._info = creator;
+  }
+
+  abstract getData(): D;
+}
